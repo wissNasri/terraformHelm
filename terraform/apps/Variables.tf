f129bbc5686b4c69048630f0f059d6cde87d1c40
wissNasri <nasri.wissen@ieee.org>
2025-09-03 20:37:28 +0100

    Update Variables.tf

--- terraform/apps/Variables.tf
+++ terraform/apps/Variables.tf
@@ -1,5 +1,12 @@
+# terraform/apps/variables.tf
 variable "cluster_name" {
-  description = "Le nom du cluster EKS cible"
+  description = "Name of the EKS cluster to target"
   type        = string
-  default     = "tws-eks-cluster" 
+  # PAS de default ici ! À fournir par l'utilisateur
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
