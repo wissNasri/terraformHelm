1d8d7f913324e9d7fbcb5385d8c9dac235fa44dd
wissNasri <nasri.wissen@ieee.org>
2025-09-04 13:32:49 +0100

    Update data.tf

--- terraform/apps/data.tf
+++ terraform/apps/data.tf
@@ -1,24 +1,12 @@
-# Fichier : terraform/apps/data.tf
+# terraform/apps/data.tf
 
-# =================================================================
-# DÉCOUVERTE DE L'INFRASTRUCTURE PARTAGÉE
-# =================================================================
-# Ce fichier utilise des "data sources" pour trouver les informations
-# de l'infrastructure de base (Cluster EKS, VPC, Région).
-
-# 2. Trouve le cluster EKS en utilisant le nom fourni dans la variable.
+# Récupère les informations du cluster EKS existant
 data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
+  name = var.eks_cluster_name
 }
 
-# 3. Trouve le VPC associé au cluster EKS découvert ci-dessus.
-data "aws_vpc" "cluster_vpc" {
-  id = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
+# Récupère l'URL du fournisseur OIDC du cluster
+# L'URL est extraite et le "https://" est retiré
+data "aws_iam_openid_connect_provider" "oidc_provider" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
-
-# 4. Génère un token d'authentification temporaire pour se connecter au cluster.
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
-# Data source pour récupérer la région AWS dynamiquement
-data "aws_region" "current" {}
