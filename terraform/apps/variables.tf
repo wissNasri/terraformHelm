2ee4814aaaaa4bf675d50b2909f1d48eeaf1e5d5
wissNasri <nasri.wissen@ieee.org>
2025-09-04 13:32:28 +0100

    Update variables.tf

--- terraform/apps/variables.tf
+++ terraform/apps/variables.tf
@@ -1,6 +1,13 @@
-# Dans variables.tf ✅
-variable "cluster_name" {
-  description = "Le nom du cluster EKS cible pour le déploiement des applications."
+# terraform/apps/variables.tf
+
+variable "aws_region" {
+  description = "La région AWS où le cluster EKS est déployé."
   type        = string
-  default     = "tws-eks-cluster"
+  default     = "us-east-1" # Assurez-vous que c'est la bonne région
+}
+
+variable "eks_cluster_name" {
+  description = "Le nom du cluster EKS cible."
+  type        = string
+  default     = "tws-eks-cluster" # Le nom de votre cluster
 }
