783ad252fd9a8cff3be4e752e477530b61df17e0
wissNasri <nasri.wissen@ieee.org>
2025-09-04 13:44:32 +0100

    Update ebs_csi_driver.tf

--- terraform/apps/ebs_csi_driver.tf
+++ terraform/apps/ebs_csi_driver.tf
@@ -1,9 +1,3 @@
-# terraform/apps/ebs_csi_driver.tf
-
-# =================================================================
-# 1. CRÉATION DU RÔLE IAM POUR LE DRIVER EBS CSI
-# =================================================================
-
 module "iam_assumable_role_with_oidc_ebs" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "~> 2.0"
@@ -16,8 +10,8 @@ module "iam_assumable_role_with_oidc_ebs" {
     Role    = "role-ebs-csi-driver"
   }
 
-  # URL OIDC dynamique (utilise les data sources du fichier data.tf)
-  provider_url = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "" )
+  provider_url = replace(data.aws_iam_openid_connect_provider.oidc_provider.url, "https://", "" )
+
 
   role_policy_arns = [
     "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
@@ -25,9 +19,6 @@ module "iam_assumable_role_with_oidc_ebs" {
 }
 
 
-# =================================================================
-# 2. DÉPLOIEMENT DU CHART HELM POUR LE DRIVER EBS CSI
-# =================================================================
 
 module "ebs_csi_driver" {
   source = "../modules/alb_controller" # Idéalement, à renommer en "helm_app"
@@ -46,13 +37,10 @@ module "ebs_csi_driver" {
     deploy        = 1
   }
 
-  # ✅ CORRECT : On utilise templatefile pour lire votre fichier YAML existant
-  # et y injecter la valeur de replicaCount.
   values = [templatefile("${path.module}/helm-values/ebs-csi-driver-2.45.1.yaml", {
     replicaCount = 1
   } )]
 
-  # On injecte l'ARN du rôle IAM, ce qui est la configuration la plus importante.
   set = [
     {
       name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
