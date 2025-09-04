377ee4b4bd1dc7f08eb43acd63aee41ac1a24fe1
wissNasri <nasri.wissen@ieee.org>
2025-09-04 15:05:17 +0100

    Update terraform.tf

--- terraform/apps/terraform.tf
+++ terraform/apps/terraform.tf
@@ -1,6 +1,4 @@
-
 terraform {
-  # 1. Déclaration des providers requis par ce projet.
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,20 +13,18 @@ terraform {
       version = ">= 2.37.1"
     }
   }
+}
 
-# Configure le provider AWS pour utiliser la région définie dans les variables
 provider "aws" {
   region = var.aws_region
 }
 
-# Configure le provider Helm pour utiliser le kubeconfig généré par `aws eks update-kubeconfig`
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
 
-# Configure le provider Kubernetes (utile pour d'autres ressources k8s si besoin)
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
