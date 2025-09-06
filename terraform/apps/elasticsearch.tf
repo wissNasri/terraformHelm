4ee508c97f0e24b3f3b4bf9c6c416c42b5e808be
wissNasri <nasri.wissen@ieee.org>
2025-09-06 14:30:52 +0100

    Create elasticsearch.tf

---
+++ terraform/apps/elasticsearch.tf
@@ -0,0 +1,21 @@
+module argocd {
+  source  = "../modules/alb_controller"
+
+  namespace  = "logging"
+  repository =  "https://helm.elastic.co"
+
+  app = {
+    name          = "elasticsearch"
+    description   = "elasticsearch"
+    version       = "8.1.3"
+    chart         = "elasticsearch"
+    force_update  = true
+    wait          = false
+    recreate_pods = false
+    deploy        = 1
+  }
+  values = [templatefile("${path.module}/helm-values/elasticsearch.yaml", {
+    serverReplicas = 1
+  })]
+
+}
