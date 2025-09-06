48b60a40effccd9d162576f71b03eb518ea57c81
wissNasri <nasri.wissen@ieee.org>
2025-09-06 22:19:40 +0100

    Update kibana.tf

--- terraform/apps/kibana.tf
+++ terraform/apps/kibana.tf
@@ -1,1 +1,20 @@
+module kibana {
+  source  = "../modules/alb_controller"
 
+  namespace  = "logging"
+  repository =  "https://helm.elastic.co"
+
+  app = {
+    name          = "kibana"
+    description   = "kibana"
+    version       = "8.5.1"
+    chart         = "kibana"
+    force_update  = true
+    wait          = false
+    recreate_pods = false
+    deploy        = 1
+  }
+  values = [file("${path.module}/helm-values/kibana.yaml")]
+
+
+}
