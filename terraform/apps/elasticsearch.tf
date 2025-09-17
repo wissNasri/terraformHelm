18fe531ae838bea7bc020e86ba5b25df6160b6aa
wissNasri <nasri.wissen@ieee.org>
2025-09-17 13:33:41 +0100

    Update elasticsearch.tf

--- terraform/apps/elasticsearch.tf
+++ terraform/apps/elasticsearch.tf
@@ -1,6 +1,11 @@
 module elasticsearch {
   source  = "../modules/alb_controller"
 
+  wait_for_completion = true
+  atomic              = true
+  cleanup_on_fail     = true
+  timeout             = 1200 # 20 minutes
+
   namespace  = "logging"
   repository =  "https://helm.elastic.co"
 
@@ -16,6 +21,6 @@ module elasticsearch {
   }
   timeout             = 900
   values = [file("${path.module}/helm-values/elasticsearch.yaml")]
-  depends_on = [module.ebs_csi_driver]
+  depends_on = [kubernetes_storage_class_v1.ebs_sc]
 
 }
