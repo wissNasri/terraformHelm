3d6139212a62033e86bfad08b52a592a98699ab9
wissNasri <nasri.wissen@ieee.org>
2025-09-17 02:05:00 +0100

    Update storageclass.tf

--- terraform/apps/storageclass.tf
+++ terraform/apps/storageclass.tf
@@ -1,3 +1,4 @@
+
 resource "kubernetes_storage_class_v1" "example" {
   metadata {
     name = "ebs-storage-class"
@@ -8,4 +9,12 @@ resource "kubernetes_storage_class_v1" "example" {
   storage_provisioner = "ebs.csi.aws.com"
   reclaim_policy      = "Delete"
   volume_binding_mode = "WaitForFirstConsumer"
+
+  # --- AJOUT : Dépendance explicite ---
+  # Ne détruit cette StorageClass qu'après la destruction des modules qui l'utilisent.
+  depends_on = [
+    module.elasticsearch,
+    # Ajoutez ici tout autre module qui utilise la persistance, par exemple Prometheus.
+    module.kube-prom-stack 
+  ]
 }
