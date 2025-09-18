b8e5fdcee37a6b3a7f60c62424f7b80ae74d5894
wissNasri <nasri.wissen@ieee.org>
2025-09-18 16:20:15 +0100

    Update kube-prom-stack.tf

--- terraform/apps/kube-prom-stack.tf
+++ terraform/apps/kube-prom-stack.tf
@@ -24,7 +24,7 @@ module kube-prom-stack {
     module.alb_controller,
     kubernetes_storage_class_v1.example,
     module.iam_assumable_role_with_oidc_alb, # <-- CORRECTION FINALE
-    module.iam_assumable_role_with_oidc_ebs  # <-- CORRECTION FINALE
+    module.iam_assumable_role_with_oidc_ebs,  # <-- CORRECTION FINALE
     module.external_dns # <-- LIGNE AJOUTÉE
 
   ]
