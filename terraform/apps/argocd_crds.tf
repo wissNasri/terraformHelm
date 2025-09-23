d2ef5417f802ae15d964c544a7c2265e107a9335
wissNasri <nasri.wissen@ieee.org>
2025-09-23 02:10:20 +0100

    Update argocd_crds.tf

--- terraform/apps/argocd_crds.tf
+++ terraform/apps/argocd_crds.tf
@@ -1,17 +1,85 @@
 # Fichier : terraform/apps/argocd_crds.tf
+# DESCRIPTION : Installe les CRDs requises par Argo CD via des ressources distinctes.
+#               Cette approche est la plus compatible et la plus fiable.
 
-# Étape 1: Rendre les templates des CRDs du chart ArgoCD sans les installer.
-data "helm_template" "argocd_crds" {
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "8.1.3"
-  
-  # Important: Ne rendre que les CRDs
-  include_crds = true
+resource "kubernetes_manifest" "crd_applications" {
+  manifest = yamldecode(<<-EOT
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  name: applications.argoproj.io
+spec:
+  group: argoproj.io
+  names:
+    kind: Application
+    listKind: ApplicationList
+    plural: applications
+    shortNames: ["app", "apps"]
+  scope: Namespaced
+  versions:
+  - name: v1alpha1
+    schema:
+      openAPIV3Schema:
+        type: object
+        x-kubernetes-preserve-unknown-fields: true
+    served: true
+    storage: true
+    subresources:
+      status: {}
+EOT
+)
 }
 
-# Étape 2: Appliquer chaque CRD rendue comme un manifeste Kubernetes.
-resource "kubernetes_manifest" "argocd_crds" {
-  for_each = { for k, v in yamlsplit(data.helm_template.argocd_crds.manifest ) : k => v if length(v) > 0 }
-  manifest = each.value
+resource "kubernetes_manifest" "crd_applicationsets" {
+  manifest = yamldecode(<<-EOT
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  name: applicationsets.argoproj.io
+spec:
+  group: argoproj.io
+  names:
+    kind: ApplicationSet
+    listKind: ApplicationSetList
+    plural: applicationsets
+    shortNames: ["appset", "appsets"]
+  scope: Namespaced
+  versions:
+  - name: v1alpha1
+    schema:
+      openAPIV3Schema:
+        type: object
+        x-kubernetes-preserve-unknown-fields: true
+    served: true
+    storage: true
+    subresources:
+      status: {}
+EOT
+)
+}
+
+resource "kubernetes_manifest" "crd_appprojects" {
+  manifest = yamldecode(<<-EOT
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  name: appprojects.argoproj.io
+spec:
+  group: argoproj.io
+  names:
+    kind: AppProject
+    listKind: AppProjectList
+    plural: appprojects
+    shortNames: ["proj"]
+  scope: Namespaced
+  versions:
+  - name: v1alpha1
+    schema:
+      openAPIV3Schema:
+        type: object
+        x-kubernetes-preserve-unknown-fields: true
+    served: true
+    storage: true
+EOT
+)
 }
