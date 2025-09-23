068c8953ce7941b0146f08e7a5775a85b43d2144
wissNasri <nasri.wissen@ieee.org>
2025-09-23 05:03:59 +0100

    Update app-of-apps.tf

--- terraform/apps/app-of-apps.tf
+++ terraform/apps/app-of-apps.tf
@@ -1,9 +1,20 @@
+# On ajoute une variable pour contrôler la création de cette ressource
+variable "deploy_app_of_apps" {
+  description = "Si true, déploie l'application racine App of Apps."
+  type        = bool
+  default     = false # Par défaut, on ne la déploie pas.
+}
+
 resource "kubernetes_manifest" "app_of_apps" {
+  # On ajoute un "count" pour activer/désactiver la ressource
+  count    = var.deploy_app_of_apps ? 1 : 0
+  provider = kubernetes
+
   manifest = {
     "apiVersion" = "argoproj.io/v1alpha1"
     "kind"       = "Application"
     "metadata" = {
-      "name"      = "app-of-apps"
+      "name"      = "app-of-apps-root"
       "namespace" = "argocd"
     }
     "spec" = {
@@ -26,7 +37,6 @@ resource "kubernetes_manifest" "app_of_apps" {
     }
   }
 
-  # S'assure qu'Argo CD est bien installé avant de créer cette ressource
   depends_on = [
     module.argocd
   ]
