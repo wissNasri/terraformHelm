71e712eab1e8788ebce8f7d5add5c996abc5830f
wissNasri <nasri.wissen@ieee.org>
2025-09-23 01:44:34 +0100

    Update kube-prom-stack.tf

--- terraform/apps/kube-prom-stack.tf
+++ terraform/apps/kube-prom-stack.tf
@@ -25,9 +25,7 @@ module kube-prom-stack {
     kubernetes_storage_class_v1.example,
     module.iam_assumable_role_with_oidc_alb, # <-- CORRECTION FINALE
     module.iam_assumable_role_with_oidc_ebs,  # <-- CORRECTION FINALE
-    module.external_dns, # <-- LIGNE AJOUTÉE
-    null_resource.wait_for_argo_crds # Ajout de la dépendance
-
+    module.external_dns
 
   ]
 
