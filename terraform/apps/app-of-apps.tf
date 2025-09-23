9d2beeb9afe9683b4563420e6f82d5499cb8f61e
wissNasri <nasri.wissen@ieee.org>
2025-09-23 01:39:52 +0100

    Create app-of-apps.tf

---
+++ terraform/apps/app-of-apps.tf
@@ -0,0 +1,36 @@
+# Fichier : terraform/apps/app-of-apps.tf
+
+resource "kubernetes_manifest" "app_of_apps_root" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "app-of-apps-root"
+      "namespace" = "argocd"
+      "finalizers" = [
+        "resources-finalizer.argocd.argoproj.io"
+      ]
+    }
+    "spec" = {
+      "project" = "default"
+      "source" = {
+        "repoURL"        = "https://github.com/wissNasri/app.git"
+        "targetRevision" = "main"
+        "path"           = "applications"
+      }
+      "destination" = {
+        "server"    = "https://kubernetes.default.svc"
+        "namespace" = "argocd"
+      }
+      "syncPolicy" = {
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
+        }
+      }
+    }
+  }
+
+  # Cette ligne garantit que l'on attend la fin de l'installation d'Argo CD
+  depends_on = [module.argocd]
+}
