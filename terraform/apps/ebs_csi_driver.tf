22848ae374f40f8522cccaa2b3027e9357cbce74
wissNasri <nasri.wissen@ieee.org>
2025-09-24 23:02:33 +0100

    Update ebs_csi_driver.tf

--- terraform/apps/ebs_csi_driver.tf
+++ terraform/apps/ebs_csi_driver.tf
@@ -1,6 +1,3 @@
-# Fichier : terraform/apps/ebs_csi_driver.tf
-# DESCRIPTION : Déploie le driver AWS EBS CSI avec les permissions RBAC corrigées.
-
 module "iam_assumable_role_with_oidc_ebs" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "~> 2.0"
@@ -13,21 +10,23 @@ module "iam_assumable_role_with_oidc_ebs" {
     Role    = "role-ebs-csi-driver"
   }
 
-  provider_url = replace(data.aws_iam_openid_connect_provider.oidc_provider.url, "https://", ""  )
+  provider_url = replace(data.aws_iam_openid_connect_provider.oidc_provider.url, "https://", "" )
+
 
   role_policy_arns = [
     "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
   ]
 }
 
-module "ebs_csi_driver" {
-  source = "../modules/alb_controller"
 
+
+module "ebs_csi_driver" {
+  source = "../modules/alb_controller" # Idéalement, à renommer en "helm_app"
   wait_for_completion = true
   atomic              = true
   timeout             = 600
-  namespace           = "kube-system"
-  repository          = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
 
   app = {
     name          = "aws-ebs-csi-driver"
@@ -47,20 +46,6 @@ module "ebs_csi_driver" {
     {
       name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
       value = module.iam_assumable_role_with_oidc_ebs.this_iam_role_arn
-    }
-  ]
-
-  # CORRECTION : Utilisation de set_list pour injecter la règle RBAC.
-  set_list = [
-    {
-      name  = "sidecars.provisioner.additionalClusterRoleRules"
-      value = [
-        yamlencode({
-          apiGroups = [""]
-          resources = ["persistentvolumes"]
-          verbs     = ["get", "list", "watch", "patch"]
-        })
-      ]
     }
   ]
 }
