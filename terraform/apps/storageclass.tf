7c65fed41ced3aedb55a7850aa3bbb3c1ae9e348
wissNasri <nasri.wissen@ieee.org>
2025-09-24 23:01:55 +0100

    Update storageclass.tf

--- terraform/apps/storageclass.tf
+++ terraform/apps/storageclass.tf
@@ -1,25 +1,17 @@
-# Fichier : terraform/apps/storageclass.tf
-# DESCRIPTION : Crée la StorageClass par défaut pour le provisionnement dynamique des volumes EBS.
-
 resource "kubernetes_storage_class_v1" "example" {
   metadata {
     name = "ebs-storage-class"
     annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
+      "storageclass.kubernetes.io/is-default-class": "true"
     }
   }
   storage_provisioner = "ebs.csi.aws.com"
   reclaim_policy      = "Delete"
   volume_binding_mode = "WaitForFirstConsumer"
 
-  # ===================================================================
-  # AJOUT CRUCIAL : Dépendance de destruction
-  # ===================================================================
-  # La dépendance à ebs_csi_driver est implicite via le nom du provisioner.
-  # On ajoute la dépendance explicite au hook de nettoyage pour forcer l'ordre de destruction.
   depends_on = [
     module.ebs_csi_driver,
-    module.iam_assumable_role_with_oidc_ebs
+    module.iam_assumable_role_with_oidc_ebs  # <-- CORRECTION FINALE
 
-]
+  ]
 }
