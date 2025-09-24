66d973f35954d5b11d263f354706ae8de0b1f6c8
wissNasri <nasri.wissen@ieee.org>
2025-09-24 20:01:23 +0100

    Update outputs.tf

--- terraform/modules/alb_controller/outputs.tf
+++ terraform/modules/alb_controller/outputs.tf
@@ -4,3 +4,12 @@ output "metadata" {
   description = "Métadonnées de la release Helm déployée."
   value       = var.app["deploy"] ? helm_release.this[0].metadata : null
 }
+output "app" {
+  description = "La carte de configuration de l'application qui a été déployée."
+  value       = var.app
+}
+
+output "namespace" {
+  description = "Le namespace dans lequel l'application a été déployée."
+  value       = var.namespace
+}
