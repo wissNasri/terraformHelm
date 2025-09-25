94831db3fa9c65ebb1ab70b2d8474a0f71097535
wissNasri <nasri.wissen@ieee.org>
2025-09-26 00:17:49 +0100

    Update variables.tf

--- terraform/apps/variables.tf
+++ terraform/apps/variables.tf
@@ -1,13 +1,20 @@
-# terraform/apps/variables.tf
+# Fichier : terraform/apps/variables.tf
 
 variable "aws_region" {
   description = "La région AWS où le cluster EKS est déployé."
   type        = string
-  default     = "us-east-1" # Assurez-vous que c'est la bonne région
+  default     = "us-east-1"
 }
 
 variable "eks_cluster_name" {
   description = "Le nom du cluster EKS cible."
   type        = string
-  default     = "tws-eks-cluster" # Le nom de votre cluster
+  default     = "tws-eks-cluster"
+}
+
+# === AJOUT DE LA NOUVELLE VARIABLE DE CONTRÔLE ===
+variable "destroy_mode" {
+  description = "Activer ce mode pour forcer la destruction des applications avant l'infrastructure."
+  type        = bool
+  default     = false
 }
