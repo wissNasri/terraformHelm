87c34d4f1535607606d62569b06db23df90686ac
wissNasri <nasri.wissen@ieee.org>
2025-09-27 05:35:02 +0100

    Update terraform.tf

--- terraform/apps/terraform.tf
+++ terraform/apps/terraform.tf
@@ -5,6 +5,8 @@ terraform {
     bucket         = "terraform-s3-backend-tws-hackathon1111"
     key            = "apps/terraform.tfstate"
     region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
   }
 
   required_providers {
